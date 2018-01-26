4e422c4b4eb02385435db72260e46969f406f553
Juan D. Jara <juanorigami@gmail.com>
2018-01-26 23:03:46 +0100

    include unity project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 3;
+    public float angularSpeed = 100;
+
+    Transform myTrans;
+    // Use this for initialization
+    void Start () {
+        myTrans = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.W)) {
+            myTrans.Translate(Vector3.forward * speed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.S)) {
+            myTrans.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+            myTrans.Rotate(Vector3.up * angularSpeed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.A)) {
+            myTrans.Rotate(Vector3.down * angularSpeed * Time.deltaTime);
+        }
+    }
+
+    /*enum class EVirusStatus{
+
+    }*/
+}
---
+++ Assets/Scripts/SampleNavScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SampleNavScript : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.position);
+
+        
+	}
+
+    private void OnCollisionEnter (Collision collision) {
+        if( collision.gameObject == target.gameObject) {
+            target.position = newTargetPosition();
+        }
+    }
+
+    Vector3 newTargetPosition () {
+
+        float x = Random.Range(-12.68f, 33.48f);
+        float z = Random.Range(1.74f, 24.84f);
+
+        return (new Vector3(x, 0, z));
+    }
+
+}
