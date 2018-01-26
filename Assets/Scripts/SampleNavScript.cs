e0b42f8ee67f9d27610b2e5f0d1407156f78e65f
Jorge Marin <jorge.1994.marin@gmail.com>
2018-01-26 23:15:20 +0100

    .
    .

--- Assets/Scripts/SampleNavScript.cs
+++ Assets/Scripts/SampleNavScript.cs
@@ -8,30 +8,31 @@ public class SampleNavScript : MonoBehaviour {
     public Transform target;
     NavMeshAgent agent;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject[] stateVirus;
+
+    // Use this for initialization
+    void Start () {
         agent = GetComponent<NavMeshAgent>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update () {
         agent.SetDestination(target.position);
 
-        
-	}
+
+    }
 
     private void OnCollisionEnter (Collision collision) {
-        if( collision.gameObject == target.gameObject) {
-            target.position = newTargetPosition();
+        if (collision.gameObject == target.gameObject) {
+            target.position = NewTargetPosition();
         }
     }
 
-    Vector3 newTargetPosition () {
+    Vector3 NewTargetPosition () {
+        float x = Random.Range(-8.14f, 8.07f);
+        float z = Random.Range(-4.4f, 3.69f);
 
-        float x = Random.Range(-12.68f, 33.48f);
-        float z = Random.Range(1.74f, 24.84f);
-
-        return (new Vector3(x, 0, z));
+        return (new Vector3(x, 0.5f, z));
     }
 
 }
