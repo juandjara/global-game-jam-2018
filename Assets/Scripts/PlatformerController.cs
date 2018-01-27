8a1930e2428a9e534fbee0379d6c06ae5043acd5
Juan D. Jara <juanorigami@gmail.com>
2018-01-27 19:12:07 +0100

    cambios del nivel de plataformas

--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -12,6 +12,7 @@ public class PlatformerController : MonoBehaviour {
 	public float raycastMargin = 0.1f;
 	public float dieLine = -2;
 	private float distToGround;
+	private float radius;
 	Rigidbody body;
 	CapsuleCollider capsule;
 
@@ -20,15 +21,21 @@ public class PlatformerController : MonoBehaviour {
 		body = GetComponent<Rigidbody>();
 		capsule = GetComponent<CapsuleCollider>();
 		distToGround = capsule.bounds.extents.y;
+		radius = capsule.bounds.extents.x;
 	}
 
 	bool isGrounded() {
 		return Physics.Raycast(transform.position, -Vector3.up, distToGround + raycastMargin);
 	}
+	bool canWallJump() {
+		bool right = Physics.Raycast(transform.position, Vector3.right, radius + raycastMargin);
+		bool left = Physics.Raycast(transform.position, Vector3.left, radius + raycastMargin);
+		return right || left;
+	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetKeyDown("space") && isGrounded()) {
+		if(Input.GetKeyDown("space") && (isGrounded() || canWallJump())) {
 			body.velocity = Vector3.up * jumpSpeed;
 		}
 		float move = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
@@ -38,7 +45,14 @@ public class PlatformerController : MonoBehaviour {
 			Die();
 		}
 	}
-	void Die() {
-		// TODO go back to top view
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "Respawn") {
+			Die();
+		}
+	}
+
+	public void Die() {
+		SceneManager.LoadScene("platform");
 	}
 }
