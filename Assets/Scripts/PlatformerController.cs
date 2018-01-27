703ca89b42694514e968a176c388aa34fd1d09cd
Juan D. Jara <juanorigami@gmail.com>
2018-01-27 19:40:12 +0100

    movimiento lateral

--- Assets/Scripts/PlatformerController.cs
+++ Assets/Scripts/PlatformerController.cs
@@ -27,18 +27,34 @@ public class PlatformerController : MonoBehaviour {
 	bool isGrounded() {
 		return Physics.Raycast(transform.position, -Vector3.up, distToGround + raycastMargin);
 	}
-	bool canWallJump() {
-		bool right = Physics.Raycast(transform.position, Vector3.right, radius + raycastMargin);
-		bool left = Physics.Raycast(transform.position, Vector3.left, radius + raycastMargin);
-		return right || left;
+	bool wallAtRight() {
+		return Physics.Raycast(transform.position, Vector3.right, radius + raycastMargin);
+	}
+	bool wallAtLeft() {
+		return Physics.Raycast(transform.position, Vector3.left, radius + raycastMargin);		
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if(Input.GetKeyDown("space") && (isGrounded() || canWallJump())) {
-			body.velocity = Vector3.up * jumpSpeed;
+		bool _isGrounded = isGrounded();
+		bool _wallAtLeft = wallAtLeft();
+		bool _wallAtRight = wallAtRight();
+		if(Input.GetKeyDown("space")) {
+			if(_isGrounded || _wallAtLeft || _wallAtRight) {
+				body.velocity = Vector3.up * jumpSpeed;
+			}
+			if(_wallAtRight) {
+				body.velocity += Vector3.left * moveSpeed / 2;
+			}
+			if(_wallAtLeft) {
+				body.velocity += Vector3.right * moveSpeed / 2;
+			}
 		}
-		float move = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+		float axis = Input.GetAxis("Horizontal");
+		float move = axis * moveSpeed * Time.deltaTime;
+		if(Mathf.Abs(axis) > 0.05 && Mathf.Abs(body.velocity.x) > 0) {
+			body.velocity.Set(0, body.velocity.y, 0);
+		}
 		transform.Translate(move, 0, 0);
 
 		if(transform.position.y < dieLine) {
